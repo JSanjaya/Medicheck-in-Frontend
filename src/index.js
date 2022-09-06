ff5f9150871a741e718b33d98fb1353485504d9c
JSanjaya <john.sanjaya999@gmail.com>
2022-09-05 17:31:35 -0700

    rid of dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "./index.css"
 import App from "./App"
 import { UserProvider } from "./components/UserContext"
 import Header from "./components/partials/Header"
